687c165f970252b0e1fd71fbb54e16f62b0d81ad
Mark Skelton <mdskelton99@gmail.com>
2024-01-15 10:48:52 -0600

    Part 2

--- 2022/day1/src/main.zig
+++ 2022/day1/src/main.zig
@@ -1,5 +1,28 @@
 const std = @import("std");
 
+fn set_max(input: *[3]i32, value: i32) void {
+    // As we step through the array, if a value is greater than the current
+    // value, we'll update the array and save the value that was there as we
+    // continue stepping through the array.
+    var current = value;
+
+    for (input, 0..) |v, i| {
+        if (current > v) {
+            const temp = input[i];
+            input[i] = current;
+            current = temp;
+        }
+    }
+}
+
+fn sum(input: [3]i32) i32 {
+    var res: i32 = 0;
+    for (input) |i| {
+        res += i;
+    }
+    return res;
+}
+
 fn calculate() !i32 {
     var file = try std.fs.cwd().openFile("src/input.txt", .{});
     defer file.close();
@@ -7,14 +30,14 @@ fn calculate() !i32 {
     var buf_reader = std.io.bufferedReader(file.reader());
     var in_stream = buf_reader.reader();
 
-    var max: i32 = 0;
+    var top_three = [3]i32{ 0, 0, 0 };
     var current: i32 = 0;
 
     var buf: [1024]u8 = undefined;
     while (try in_stream.readUntilDelimiterOrEof(&buf, '\n')) |line| {
         // If the line is empty, we've reached the end of a group.
         if (line.len == 0) {
-            max = @max(current, max);
+            set_max(&top_three, current);
             current = 0;
         } else {
             current += try std.fmt.parseInt(i32, line, 10);
@@ -22,9 +45,9 @@ fn calculate() !i32 {
     }
 
     // We need to check the last group, since it won't have a trailing newline.
-    max = @max(current, max);
+    set_max(&top_three, current);
 
-    return max;
+    return sum(top_three);
 }
 
 pub fn main() !void {
