c418a328d3956e0007020dd695b4b1eadca59733
Mark Skelton <mdskelton99@gmail.com>
2024-01-15 10:30:47 -0600

    Part 1

--- 2022/day1/src/main.zig
+++ 2022/day1/src/main.zig
@@ -1,19 +1,37 @@
 const std = @import("std");
 
-pub fn main() !void {
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
+fn calculate() !i32 {
+    var file = try std.fs.cwd().openFile("src/input.txt", .{});
+    defer file.close();
 
-    const stdout = bw.writer();
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
+    var buf_reader = std.io.bufferedReader(file.reader());
+    var in_stream = buf_reader.reader();
 
-    try bw.flush();
+    var max: i32 = 0;
+    var current: i32 = 0;
+
+    var buf: [1024]u8 = undefined;
+    while (try in_stream.readUntilDelimiterOrEof(&buf, '\n')) |line| {
+        // If the line is empty, we've reached the end of a group.
+        if (line.len == 0) {
+            max = @max(current, max);
+            current = 0;
+        } else {
+            current += try std.fmt.parseInt(i32, line, 10);
+        }
+    }
+
+    // We need to check the last group, since it won't have a trailing newline.
+    max = @max(current, max);
+
+    return max;
 }
 
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit();
+pub fn main() !void {
+    const result = try calculate();
+    std.debug.print("Result: {d}\n", .{result});
+}
 
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
+test "calculates the correct answer" {
+    try std.testing.expectEqual(@as(i32, 24000), try calculate());
 }
--- README.md
+++ README.md
@@ -2,6 +2,15 @@
 
 My submissions for advent of code. Never stop learning!
 
+## 2022
+
+2022 was focused on learning Zig.
+
+```bash
+cd 2022/day1
+zig build run
+```
+
 ## 2023
 
 2023 was combination of teaching my buddy Rust and for myself learning Scala.
